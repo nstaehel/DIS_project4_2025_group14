823a1ffe17a2346a2b995bb0d845ffb493d127c0
nstaehel <noe.staeheli@epfl.ch>
2025-10-30 10:18:27 +0100

    Simplified structure

--- Code/Project1/controllers/e-puck/build/release/e-puck.d
+++
@@ -1,4 +0,0 @@
-build/release/e-puck.o: e-puck.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
