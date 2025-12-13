737d50c0902e3cd36dafca7bb9d1c4c1a23daeb2
FedericoVassalloEpfl <federico.vassallo@epfl.ch>
2025-12-13 17:13:19 +0100

    Implement death check and update robot parameters
    Added death time and steps for robots, updated allocation for both robot types, and included a death check in the simulation loop.

--- macro_modelling.py
+++ macro_modelling.py
@@ -1,53 +1,76 @@
 import numpy as np
 
-MAX_RUNTIME = 3*60 
-DELTA_T = 1/3
-
+MAX_RUNTIME = 3 * 60 
+DELTA_T = 1 / 3
+DEATH_TIME = 2 * 60
+DEATH_STEPS = int(DEATH_TIME / DELTA_T)
 
 for i in range(2):
-    if i==0: #type 1 robots
-        P_ALLOC = 0.5
-        N_MOVING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WAITING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WORKING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WAITING[0]=2
+    # --- SETUP PARAMETERS ---
+    if i == 0: # TYPE 1 ROBOTS
+        TOTAL_ROBOTS = 2
+        P_ALLOC =  1 # Changed to 1 as requested
         tau_work = 43
         tau_travel = round(0.6525/0.5/DELTA_T)
-        for k in range(int(MAX_RUNTIME/DELTA_T)):
-            if k - tau_travel < 0:
-                k_delta_t = 0
-            else:
-                k_delta_t = k - tau_travel
-            N_MOVING[k+1] = N_MOVING[k] + P_ALLOC * N_WAITING[k] - P_ALLOC * N_WAITING[k_delta_t]
-            if k - tau_work - tau_travel < 0:
-                k_delta_work = 0
-            else:
-                k_delta_work = k - tau_work - tau_travel
-            N_WORKING[k+1] = N_WORKING[k] + P_ALLOC * N_WAITING[k_delta_t] - P_ALLOC * N_WAITING[k_delta_work]
-            N_WAITING[k+1] = 2 - N_WORKING[k] - N_MOVING[k]
-        N_ACTIVE = N_MOVING + N_WORKING
-        average_type1 = np.mean(N_ACTIVE)
-    else:
-        P_ALLOC = 0.5
-        N_MOVING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WAITING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WORKING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
-        N_WAITING[0]=3
+    else: # TYPE 2 ROBOTS
+        TOTAL_ROBOTS = 3
+        P_ALLOC = 1 # Changed to 1 as requested
         tau_work = 37
         tau_travel = round(0.6525/0.5/DELTA_T)
-        for k in range(int(MAX_RUNTIME/DELTA_T)):
-            if k - tau_travel < 0:
-                k_delta_t = 0
-            else:
-                k_delta_t = k - tau_travel
-            N_MOVING[k+1] = N_MOVING[k] + P_ALLOC * N_WAITING[k] - P_ALLOC * N_WAITING[k_delta_t]
-            if k - tau_work - tau_travel < 0:
-                k_delta_work = 0
-            else:
-                k_delta_work = k - tau_work - tau_travel
-            N_WORKING[k+1] = N_WORKING[k] + P_ALLOC * N_WAITING[k_delta_t] - P_ALLOC * N_WAITING[k_delta_work]
-            N_WAITING[k+1] = 2 - N_WORKING[k] - N_MOVING[k]
-        N_ACTIVE = N_MOVING + N_WORKING
-        average_type2 = np.mean(N_ACTIVE)
+
+    # Initialize Arrays
+    N_MOVING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
+    N_WAITING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
+    N_WORKING = np.zeros(int(MAX_RUNTIME/DELTA_T)+2)
+    N_WAITING[0] = TOTAL_ROBOTS
+
+    # --- SIMULATION LOOP ---
+    for k in range(int(MAX_RUNTIME/DELTA_T)):
         
-print("Average for type 1: ", average_type1," ; Average for type 2: ", average_type2, "; Overall average: ", 0.5*(average_type1+average_type2))
+        # [NEW] DEATH CHECK
+        # We check how many steps have been active so far.
+        # We slice [:k] to only count the past.
+        active_steps_so_far = np.count_nonzero((N_MOVING[:k] + N_WORKING[:k]) > 0.001)
+        
+        if active_steps_so_far > DEATH_STEPS:
+            # If we exceeded the battery life, stop the loop.
+            # The remaining values in the arrays will stay 0.
+            break
+        
+        # --- FLOW CALCULATION (Fixes Negative Values) ---
+        
+        # 1. Flow: Moving -> Working
+        if k - tau_travel < 0:
+            term_arrival_work = 0
+        else:
+            k_delta_t = k - tau_travel
+            term_arrival_work = P_ALLOC * N_WAITING[k_delta_t]
+
+        # Update Moving
+        N_MOVING[k+1] = N_MOVING[k] + P_ALLOC * N_WAITING[k] - term_arrival_work
+        
+        # 2. Flow: Working -> Waiting
+        if k - tau_work - tau_travel < 0:
+            term_finish_work = 0
+        else:
+            k_delta_work = k - tau_work - tau_travel
+            term_finish_work = P_ALLOC * N_WAITING[k_delta_work]
+
+        # Update Working
+        N_WORKING[k+1] = N_WORKING[k] + term_arrival_work - term_finish_work
+        
+        # 3. Update Waiting (Conservation of Mass)
+        N_WAITING[k+1] = TOTAL_ROBOTS - N_WORKING[k+1] - N_MOVING[k+1]
+
+    # --- SAVE RESULTS ---
+    N_ACTIVE = N_MOVING + N_WORKING
+    
+    if i == 0:
+        average_type1 = np.mean(N_ACTIVE)
+    else:
+        average_type2 = np.mean(N_ACTIVE)
+
+# Print Final
+print("Average for type 1: ", average_type1)
+print("Average for type 2: ", average_type2)
+print("Overall average:    ", (average_type1+average_type2))
